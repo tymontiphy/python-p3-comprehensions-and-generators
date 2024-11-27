9dd95648e3f3a50ca34bc069ec410d561ab2ea0d
Your Name <youremail@domain.com>
2024-11-28 02:37:49 +0300

    first commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    '''Returns a new list containing only the even numbers from num_list.'''    
+    return [num for num in num_list if num % 2 == 0]
     pass
 
 def make_exclamation(sentence_list):
+    '''Returns a new list containing sentences with an exclamation mark at the end.'''
+    return [sentence + '!' for sentence in sentence_list]
     pass
